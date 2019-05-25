f6928eff308b81e0e1fafdd40dfdbd6a69a590f7
Kaaden <794102675@qq.com>
2019-05-25 11:32:48 +0800

    Update index.js

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -2,6 +2,9 @@ import { connect } from "dva"
 import { Component } from "react"
 import { Head, List, Navigator, Introduct, Footer } from "../../components"
 class Index extends Component {
+  open = () => {
+    window.open('https://github.com/login/oauth/authorize\?client_id=a81d2df07a5f4265c4a0', '', 'width=600,height=500,left=10, top=10,toolbar=no, status=no, menubar=no, resizable=yes, scrollbars=yes');
+  }
   render() {
     return (
       <div className="container">
@@ -9,6 +12,9 @@ class Index extends Component {
         <Navigator />
         <Head />
         <div className="container-main">
+          <iframe src='https://github.com/login/oauth/authorize\?client_id=a81d2df07a5f4265c4a0' width="300"
+            height="200"></iframe>
+          <div onClick={this.open}>打开</div>
           <List />
           <Introduct />
         </div>
